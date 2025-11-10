410fb0ae86326ceabd942f86ac2c7ae17c04e9ee
umerrais006 <umerrais006@icloud.com>
2025-11-10 17:38:34 +0500

    remove unecessary comments

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -11,30 +11,15 @@ const {
 } = require("../controllers/users.controller");
 
 const { userValidationMiddileware } = require("../middleware/users.middleware");
-// const { findByIdAndDelete } = require("../models/user.model");
 const router = express.Router();
-
-// router.get("/api/users/email/:email", (req, res, next) => {
-//   console.log("jejeje");
-//   next();
-// });
 
 router.post("/api/users", userValidationMiddileware, createUser);
 router.get("/api/users/search", getUserByAge);
 router.get("/api/users/details/search", getUserByAgeLesser);
 router.get("/api/users", getAllUser);
-
 router.get("/api/users/:id", getUserById);
 router.put("/api/users/:id", userValidationMiddileware, updateUserByID);
 router.delete("/api/users/:id", findUserAndDelete);
-
 router.get("/api/users/email/:email", getUserByEmail);
-// router.get(
-//   "/api/users/email/:email",
-//   (req, res, next) => {
-//     console.log("heheheh!@##4");
-//   },
-//   getUserByEmail
-// );
 
 module.exports = router;
